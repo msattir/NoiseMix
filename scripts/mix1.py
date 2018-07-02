cecfefa1ac6ea10baf38b9a8ad6c45d764a23326
Mayukh Sattiraju <msattir@clmeosn.edu>
2018-07-02 00:23:44 -0400

    Noise and Audio mixing scripts

---
+++ scripts/mix1.py
@@ -0,0 +1,11 @@
+from scipy.io.wavfile import read
+import numpy as np
+
+noise_file = "/home/carla/msattir/noise_mix/audio_samples/noise/factory1.wav"
+audio_file = "/home/carla/msattir/noise_mix/audio_samples/audio/sw2263A-ms98-a-trans-108.670875-110.07625.wav"
+
+noise = np.array(read(noise_file)[1], dtype=float)
+audio = np.array(read(audio_file)[1], dtype=float)
+
+
+
